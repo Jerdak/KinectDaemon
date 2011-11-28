925055ce546c07255fbe1d5011b6eaa7fe7a4a90
jerdak_win7 <jerdak@gmail.com>
2011-11-28 00:58:46 -0500

    Add features to support depth writing as mesh
    -  Add depthPixel buffer, store frame image pixels per tick
    -  Modify convertDepthFrame() to use ImageFrame rather than Image.  This will provide more downstream info. without having to create N more class variables
    -  Add switch to convertDepthFrame to handle depths from different image types (mirrored vs. not)
    -  Create convertRealDepth() function to convert depthPixels in to 3D vertices.
    -  Create DumpData() function to write 3D vertex data to a Wavefront .obj file.  Ignore faces, normals, and color for now.  Dumpdata() will write data to numerically consecutive files.

--- KinectDaemon/UserInterface/UIDepthViewer.xaml.cs
+++ KinectDaemon/UserInterface/UIDepthViewer.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,7 +23,12 @@ namespace KinectDaemon.UserInterface
     public partial class UIDepthViewer : UserControl
     {
         Runtime _kinectRuntime = null;
+
+        int depthWidth = 0;
+        int depthHeight = 0;
+        int[] depthPixels = new int[320 * 240];
         byte[] depthFrame32 = new byte[320 * 240 * 4];
+   
         InteropBitmapHelper imageHelper = null;
         const int RED_IDX = 2;
         const int GREEN_IDX = 1;
@@ -46,8 +53,9 @@ namespace KinectDaemon.UserInterface
         }
         // Converts a 16-bit grayscale depth frame which includes player indexes into a 32-bit frame
         // that displays different players in different colors
-        byte[] convertDepthFrame(byte[] depthFrame16)
+        byte[] convertDepthFrame(ImageFrame Image)
         {
+            byte[] depthFrame16 = Image.Image.Bits;
             for (int i16 = 0, i32 = 0; i16 < depthFrame16.Length && i32 < depthFrame32.Length; i16 += 2, i32 += 4)
             {
                 int player = depthFrame16[i16] & 0x07;
@@ -56,10 +64,22 @@ namespace KinectDaemon.UserInterface
                 // for display (we disregard information in most significant bit)
                 byte intensity = (byte)(255 - (255 * realDepth / 0x0fff));
 
+                switch (Image.Type)
+                {
+                    case ImageType.DepthAndPlayerIndex:
+                        depthPixels[i16 / 2] = (depthFrame16[i16 + 1] << 5) | (depthFrame16[i16] >> 3);
+                        break;
+                    case ImageType.Depth:
+                        //Mirror depth
+                        depthPixels[i16 / 2] = (depthFrame16[i16 + 1] << 8) | depthFrame16[i16];
+                        break;
+                }
+               
                 depthFrame32[i32 + RED_IDX] = 0;
                 depthFrame32[i32 + GREEN_IDX] = 0;
                 depthFrame32[i32 + BLUE_IDX] = 0;
 
+               
                 // choose different display colors based on player
                 switch (player)
                 {
@@ -103,12 +123,44 @@ namespace KinectDaemon.UserInterface
             }
             return depthFrame32;
         }
+        List<Microsoft.Research.Kinect.Nui.Vector> convertRealDepth()
+        {
+            List<Microsoft.Research.Kinect.Nui.Vector> ret = new List<Microsoft.Research.Kinect.Nui.Vector>();
+            for (int i = 0; i < depthPixels.Length; i+=2)
+            {
+                int depthPixel = depthPixels[i/2];
 
+                // The x and y positions can be calculated using modulus
+                // division from the array index
+
+                int x = (i / 2) % depthWidth;
+                int y = (i / 2) / depthWidth;
+
+                // The x and y we pass into DepthImageToSkeleton() need to
+                // be normalised (between 0 and 1), so we divide by the
+                // width and height of the depth image, respectively
+
+                // As we're using UseDepth (not UseDepthAndPlayerIndex) in
+                // the depth sensor settings, we also need to shift the
+                // depth pixel by 3 bits
+
+               Microsoft.Research.Kinect.Nui.Vector v = _kinectRuntime.SkeletonEngine.DepthImageToSkeleton(
+                    ((float)x) / ((float)depthWidth),
+                    ((float)y) / ((float)depthHeight),
+                    (short)(depthPixel << 3)
+                  );
+               if(v.Z>0)ret.Add(v);
+            }
+            return ret;
+        }
         void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e)
         {
             PlanarImage Image = e.ImageFrame.Image;
-            byte[] convertedDepthFrame = convertDepthFrame(Image.Bits);
 
+            byte[] convertedDepthFrame = convertDepthFrame(e.ImageFrame);
+            depthWidth = Image.Width;
+            depthHeight = Image.Height;
+           
             //An interopBitmap is a WPF construct that enables resetting the Bits of the image.
             //This is more efficient than doing a BitmapSource.Create call every frame.
             if (imageHelper == null)
@@ -119,6 +171,31 @@ namespace KinectDaemon.UserInterface
             else
             {
                 imageHelper.UpdateBits(convertedDepthFrame);
+            }
+        }
+
+        public void DumpData()
+        {
+            List<Microsoft.Research.Kinect.Nui.Vector> verts = convertRealDepth();
+            DirectoryInfo root = new DirectoryInfo("./Dump");
+            if(!root.Exists){
+                root.Create();
+            }
+
+            int ct = 1;
+            FileInfo file = new FileInfo(root.FullName + "/" + "dump_0.obj");
+            while(file.Exists){
+                file = new FileInfo(root.FullName + "/" + "dump_" + ct.ToString() + ".obj");
+                ct++;
+            }
+
+            using (StreamWriter sw = new StreamWriter(file.FullName))
+            {
+                foreach (Microsoft.Research.Kinect.Nui.Vector v in verts)
+                {
+                    sw.WriteLine("v " + v.X.ToString() + " " + v.Y.ToString() + " " + v.Z.ToString());
+                }
+               
             }
         }
     }
