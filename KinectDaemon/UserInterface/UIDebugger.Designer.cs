92d8e3472f6187f348dc65ff45c6370015f7647a
jerdak_win7 <jerdak@gmail.com>
2011-11-28 01:01:31 -0500

    Add support for depth dump debugging
    - Add 'Dump' and 'Dump5' buttons.

--- KinectDaemon/UserInterface/UIDebugger.Designer.cs
+++ KinectDaemon/UserInterface/UIDebugger.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.uiDepthViewer = new KinectDaemon.UserInterface.UIDepthViewer();
+            this.btDumpDepth = new System.Windows.Forms.Button();
+            this.btDump5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // elementHost1
@@ -41,11 +43,33 @@
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = this.uiDepthViewer;
             // 
+            // btDumpDepth
+            // 
+            this.btDumpDepth.Location = new System.Drawing.Point(15, 318);
+            this.btDumpDepth.Name = "btDumpDepth";
+            this.btDumpDepth.Size = new System.Drawing.Size(76, 23);
+            this.btDumpDepth.TabIndex = 1;
+            this.btDumpDepth.Text = "Dump";
+            this.btDumpDepth.UseVisualStyleBackColor = true;
+            this.btDumpDepth.Click += new System.EventHandler(this.btDumpDepth_Click);
+            // 
+            // btDump5
+            // 
+            this.btDump5.Location = new System.Drawing.Point(97, 318);
+            this.btDump5.Name = "btDump5";
+            this.btDump5.Size = new System.Drawing.Size(76, 23);
+            this.btDump5.TabIndex = 2;
+            this.btDump5.Text = "Dump (5s)";
+            this.btDump5.UseVisualStyleBackColor = true;
+            this.btDump5.Click += new System.EventHandler(this.btDump5_Click);
+            // 
             // UIDebugger
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 319);
+            this.ClientSize = new System.Drawing.Size(327, 365);
+            this.Controls.Add(this.btDump5);
+            this.Controls.Add(this.btDumpDepth);
             this.Controls.Add(this.elementHost1);
             this.Name = "UIDebugger";
             this.Text = "UIDebugger";
@@ -57,6 +81,8 @@
 
         private System.Windows.Forms.Integration.ElementHost elementHost1;
         private UIDepthViewer uiDepthViewer;
+        private System.Windows.Forms.Button btDumpDepth;
+        private System.Windows.Forms.Button btDump5;
 
     }
 }
